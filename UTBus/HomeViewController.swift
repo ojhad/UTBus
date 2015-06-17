a7b430c7ae52bb37d9729a9e713e920e798a77cf
Rodolfo Martinez <rodolfo.martinez@mail.utoronto.ca>
2015-06-17 14:40:57 -0400

    added array of monday

--- UTBus/HomeViewController.swift
+++ UTBus/HomeViewController.swift
@@ -14,85 +14,41 @@ class HomeViewController: UIViewController , UITableViewDelegate, UITableViewDat
     
     let textCellIdentifier = "TextCell"
     
-    var swiftBlogs = ["Ray Wenderlich", "NSHipster", "iOS Developer Tips", "Jameson Quave", "Natasha The Robot", "Coding Explorer", "That Thing In Swift", "Andrew Bancroft", "iAchieved.it", "Airspeed Velocity"]
-    
+    var source: dataParser!
+    var days: NSArray = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-        /*
-        let bundle = NSBundle.mainBundle()
-        let URL: NSURL? = bundle.URLForResource("data", withExtension: ".json");
-        let data: NSData? = NSData(contentsOfURL: URL!)
-
-        var parseError: NSError?
-        let parsedObject: AnyObject? = NSJSONSerialization.JSONObjectWithData(data!,
-            options: NSJSONReadingOptions.AllowFragments,
-            error:&parseError)
         
-        //println("Printing Time: \(parsedObject)")
-        
-        if let main = parsedObject as? NSDictionary {
-            println("Success Route\n")
-            if let route = main["mississauga"] as? NSDictionary {
-                println("Success day\n")
-                if let day = route["monday"] as? NSArray{
-                    println("Success time\n")
-                    if let time = day[22] as? NSDictionary{
-                        println("Success time2\n")
-                        if let timeVal = time["time"] as? NSString{
-                            println("First Time: \(time)")
-                        }
-                    }
-                }
-            }
-        }*/
-        
-        let source: dataParser = dataParser.new()
-        
-        let days: NSArray = source.getArrayOfTimesForDay("monday")
-        
-        //let time: String = days[0] as! String
-        
-        let time1: NSDictionary? = days[0] as? NSDictionary
-        
-        
-        println("\(time1)")
-        
-        let time = time1["time"]
-        
-        //swiftBlogs.append(time)
+        source = dataParser.new()
+        days=source.getArrayOfTimesForDay("monday")
         
         
         tableView.delegate = self
         tableView.dataSource = self
     }
     
-    
-    // MARK:  UITextFieldDelegate Methods
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return swiftBlogs.count
+        return days.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath) as! UITableViewCell
         
         let row = indexPath.row
-        cell.textLabel?.text = swiftBlogs[row] as? String
-        
+        cell.textLabel?.text = days[row] as? String
+        //println("\(days[row])")
         return cell
     }
     
-    // MARK:  UITableViewDelegate Methods
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        
+
         let row = indexPath.row
-        println(swiftBlogs[row])
     }
 }
